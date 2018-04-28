e85b43ed8396554bc2b4d7de2985ff1b9dfe8f9e
Thomas <t.grussenmeyer@gmail.com>
2018-04-28 18:30:19 +0200

    Autostopp nach 35 Sekunden.

--- connect.sh
+++ connect.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mpfshell -o ttyUSB0 --reset -n -c 'put main.py'
 mpfshell -o ttyUSB0 --reset -c repl
 
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import machine
+import time
 
 eins = machine.Pin(14, machine.Pin.OUT)
 zwei = machine.Pin(27, machine.Pin.OUT)
 
-def rauf():
+def hoch():
     eins.value(1)
     zwei.value(0)
+    time.sleep(35)
+    zwei.value(1)
+    
 
 def runter():
     eins.value(0)
     zwei.value(1)
+    time.sleep(35)
+    eins.value(1)
 
 def stop():
     eins.value(1)
